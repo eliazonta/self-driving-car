fa4289984cae20d9430989efac4dd6a15d8a0e18
happyeliia <78420183+eliazonta@users.noreply.github.com>
2022-05-18 22:27:57 +0200

    Update sensor.js

--- sensor.js
+++ sensor.js
@@ -1,25 +1,29 @@
 class Sensor{
     constructor(car){
         this.car=car;
-        this.rayCount=6;
-        this.rayLength=100;
+        this.rayCount=5;
+        this.rayLength=150;
         this.raySpread=Math.PI/2;
 
         this.rays=[];
         this.readings=[];
     }
 
-    update(roadBorders){
+    update(roadBorders,traffic){
         this.#castRays();
         this.readings=[];
         for(let i=0;i<this.rays.length;i++){
             this.readings.push(
-                this.#getReading(this.rays[i],roadBorders)
+                this.#getReading(
+                    this.rays[i],
+                    roadBorders,
+                    traffic
+                )
             );
         }
     }
 
-    #getReading(ray,roadBorders){
+    #getReading(ray,roadBorders,traffic){
         let touches=[];
 
         for(let i=0;i<roadBorders.length;i++){
@@ -34,11 +38,26 @@ class Sensor{
             }
         }
 
+        for(let i=0;i<traffic.length;i++){
+            const poly=traffic[i].polygon;
+            for(let j=0;j<poly.length;j++){
+                const value=getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if(value){
+                    touches.push(value);
+                }
+            }
+        }
+
         if(touches.length==0){
             return null;
         }else{
             const offsets=touches.map(e=>e.offset);
-            const minOffset=Math.min(...offsets);  // ... works to spread the array into many different values
+            const minOffset=Math.min(...offsets);
             return touches.find(e=>e.offset==minOffset);
         }
     }
